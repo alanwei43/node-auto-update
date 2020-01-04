d0d0ca693cc21804fe51d471d6bd8ea7bb49172c
Alan <alan@Alans-MacBook-Pro.local>
2020-01-04 19:03:22 +0800

    build: 增加脚本

---
+++ scripts/docker-pub.sh
@@ -0,0 +1,6 @@
+# docker login docker.pkg.github.com --username alanwei43
+docker tag node-auto-update:$1 docker.pkg.github.com/alanwei43/alan.largefileupload/node-auto-update:$1
+docker push docker.pkg.github.com/alanwei43/alan.largefileupload/node-auto-update:$1
+
+docker tag node-auto-update:$1 alanway/node-auto-update:$1
+docker push alanway/node-auto-update:$1
