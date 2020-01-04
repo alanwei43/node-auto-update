c3f1deed8a0c11a3c6b81d36ec322cf0d9d04989
Alan <alan@Alans-MacBook-Pro.local>
2020-01-04 22:40:07 +0800

    feat: 增加返回系统信息

--- src/lib/WebService.ts
+++ src/lib/WebService.ts
@@ -1,19 +1,21 @@
 import http from "http";
 import { AppContainer } from "./AppContainer";
 import { Logger } from "./Log";
+import path from "path";
 
 export async function createServer(port: number, app: AppContainer) {
     http.createServer((req, res) => {
         res.writeHead(200, {
             "Content-Type": "application/json"
         });
-        if (req.url === "/status") {
-        }
         if (req.url === "/update") {
             app.update();
         }
         res.end(JSON.stringify({
-            status: "ok"
+            status: "ok",
+            date: new Date().toISOString(),
+            cwd: process.cwd(),
+            dirname: path.join(__dirname)
         }));
     }).listen(port, () => {
         Logger.debug(`listen ${port}`);
