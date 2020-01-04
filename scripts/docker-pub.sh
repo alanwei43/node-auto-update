6cff09750ace62073f9351bb8f7f986b87aaeade
Alan <alan@Alans-MacBook-Pro.local>
2020-01-04 19:39:04 +0800

    build: update

--- scripts/docker-pub.sh
+++ scripts/docker-pub.sh
@@ -1,6 +1,6 @@
 # docker login docker.pkg.github.com --username alanwei43
-docker tag node-auto-update:$1 docker.pkg.github.com/alanwei43/node-auto-update/node-auto-update:$1
-docker push docker.pkg.github.com/alanwei43/node-auto-update/node-auto-update:$1
+docker tag node-auto-update:$1 docker.pkg.github.com/alanwei43/node-auto-update/docker:$1
+docker push docker.pkg.github.com/alanwei43/node-auto-update/docker:$1
 
 docker tag node-auto-update:$1 alanway/node-auto-update:$1
 docker push alanway/node-auto-update:$1
